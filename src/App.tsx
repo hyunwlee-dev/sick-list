5d7030a56510f48f7fdcb9619b27fbf07da2b45e
hyunwlee-dev <hwlee3928@gmail.com>
2023-09-06 19:00:13 +0900

    init: begin project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Disesaes from './pages/Disesaes';
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Disesaes />
+    </>
+  );
+}
+
+export default App;
