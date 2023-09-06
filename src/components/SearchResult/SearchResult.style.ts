999b6e30e348562ab615d4e7571f30c9d9afb611
hyunwlee-dev <hwlee3928@gmail.com>
2023-09-07 03:58:45 +0900

    style: SearchResult component 스타일링

--- src/components/SearchResult/SearchResult.style.ts
+++ src/components/SearchResult/SearchResult.style.ts
@@ -8,3 +8,29 @@ export const Ul = styled.ul`
   padding: 1rem 2rem;
   border-radius: 2rem;
 `;
+
+export const Li = styled.li`
+  padding: 0.5rem 1rem;
+  display: flex;
+  flex-flow: row nowrap;
+  gap: 1rem;
+  cursor: pointer;
+  &:hover {
+    background: #e2e2e2;
+  }
+`;
+
+export const P = styled.p`
+  color: #919191;
+  margin: 1rem;
+`;
+
+export const ImageWrapper = styled.div`
+  width: 2rem;
+  height: 2rem;
+  background: #212121;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
--- src/components/SearchResult/index.tsx
+++ src/components/SearchResult/index.tsx
@@ -12,9 +12,17 @@ export const SearchResult = ({ isFocused, searchedArr, isLoading }: IProps) => {
   if (isLoading) return <>Loading...</>;
   return (
     <S.Ul>
+      <S.P>추천 검색어</S.P>
       {searchedArr.length < 1 && <>최근 검색어가 없습니다.</>}
       {searchedArr.length >= 1 &&
-        searchedArr?.map(({ sickCd, sickNm }) => <li key={sickCd}>{sickNm}</li>)}
+        searchedArr?.map(({ sickCd, sickNm }) => (
+          <S.Li key={sickCd}>
+            <S.ImageWrapper>
+              <img src="/search.svg" width={16} height={16} alt="search icon" />
+            </S.ImageWrapper>
+            <span>{sickNm}</span>
+          </S.Li>
+        ))}
     </S.Ul>
   );
 };
